554e148cde81de910ca667ceb4462c3db023d377
Jacob Walton <jawalton6616@gmail.com>
2020-07-12 11:48:43 -0500

    created schema

---
+++ models/workout.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    required: "Select cardio or resistance",
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for for your entry",
+  },
+  distance: {
+    type: String,
+    trim: true,
+    required: "Enter distance for cardio exercise",
+  },
+  weight: {
+    type: String,
+    trim: true,
+    required: "Enter a weight for resistance exercise",
+  },
+  sets: {
+    type: Number,
+    required: "Enter amount of sets",
+  },
+
+  reps: {
+    type: Number,
+    required: "Enter amount of reps",
+  },
+  duration: {
+    type: Number,
+    required: "Enter amount duration of exercise",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
